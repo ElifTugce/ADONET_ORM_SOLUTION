45779dea458d7f0be955801f0c50f602d8328349
ElifTugce <eliftugcedemir.23@gmail.com>
2021-12-23 14:39:52 +0300

    Add project files.

---
+++ ADONET_ORM_Entities/ProgramBilgileri.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADONET_ORM_Entities
+{
+    public static class ProgramBilgileri
+    {
+        public static int TurYokSelectedValue { get; set; } = -1;
+        public static void CombonunSelectedIndexiniResetle(ComboBox c)
+        {
+            c.SelectedIndex = -1;
+        }
+    }
+}
